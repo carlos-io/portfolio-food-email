6b2cd23301ec2e827a477fd0632bb07d02c24533
Carlos Velazquez <carlosjvelazquez@gmail.com>
2019-11-02 11:48:14 -0400

    initial setup

---
+++ gulpfile.js
@@ -0,0 +1,60 @@
+const { src, dest, series, parallel, watch } = require('gulp');
+const imagemin = require('gulp-imagemin');
+const postcss = require('gulp-postcss');
+const rename = require('gulp-rename');
+const sass = require('gulp-sass');
+const sourcemaps = require('gulp-sourcemaps');
+const uglify = require('gulp-uglify');
+const autoprefixer = require('autoprefixer');
+const browserSync = require('browser-sync').create();
+const cssnano = require('cssnano');
+
+
+function serve() {
+    browserSync.init({
+        server: { baseDir: './public' },
+        notify: false,
+        open: false
+    });
+
+    watch('assets/scss/**/*.scss', generateCSS);
+    watch('assets/js/**/*.js', generateJS);
+    watch('public/**/*.html').on('change', browserSync.reload);
+}
+
+
+function generateCSS() {
+    return src('assets/scss/**/*.scss')
+        .pipe(sourcemaps.init({ largeFile: true }))
+        .pipe(sass().on('error', sass.logError))
+        .pipe(postcss([ autoprefixer(), cssnano() ]))
+        .pipe(rename({ extname: '.min.css' }))
+        .pipe(sourcemaps.write('.'))
+        .pipe(dest('public/css/'))
+        .pipe(browserSync.stream());
+}
+
+
+function generateJS() {
+    return src('assets/js/**/*.js')
+        .pipe(sourcemaps.init({ largeFile: true }))
+        .pipe(uglify())
+        .pipe(rename({ extname: '.min.js' }))
+        .pipe(sourcemaps.write('.'))
+        .pipe(dest('public/js/'));
+}
+
+
+function generateIMG() {
+    return src('assets/img/**/*')
+        .pipe(imagemin({ verbose: true }))
+        .pipe(dest('public/img/'));
+}
+
+
+exports.watch = exports.serve = serve;
+exports.default = series(parallel(
+    generateCSS,
+    generateJS,
+    generateIMG
+))
